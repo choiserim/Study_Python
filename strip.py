9ac86c5f4f8ef222daf27cd432c51c23e27efbee
cloud39 <choiserim@hanmail.net>
2022-09-08 21:59:10 +0900

    source code

---
+++ strip.py
@@ -0,0 +1,13 @@
+# strip() - 양옆에 지정한 매개변수 제거
+# lstrip() - 왼쪽에 지정한 매개변수 제거
+# rstrip() - 오른쪽에 지정한 매개변수 제거
+
+text = '0000000Water boils at 100 degrees 000'
+
+print(text.strip('0'))
+print(text.lstrip('0'))
+print(text.rstrip('0'))
+
+
+
+
---
+++ upper_and_lower.py
@@ -0,0 +1,6 @@
+# upper() - 소문자를 대문자로
+# lower() - 대문자를 소문자로
+
+a = "Hello Python Programming" 
+print(a.upper())
+print(a.lower())
